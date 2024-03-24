783c670af9dee018ab6a2efd51d770c776117246
AlicjaCiepielska <alicjaciepielska@gmail.com>
2024-03-24 14:04:11 +0100

    setup

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import time
+service = Service(executable_path="chromedriver.exe")
+driver = webdriver.Chrome(service = service)
+driver.get("https://www.youtube.com/watch?v=NB8OceGZGjA&ab_channel=TechWithTim")
+time.sleep(10)
+driver.quit()
